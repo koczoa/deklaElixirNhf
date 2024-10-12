953151f7bbae8b50cd4904c62ee9958a49a3c030
koczoa <onokoczo@protonmail.com>
2024-10-12 18:16:17 +0200

    im done

--- nhf.ex
+++ nhf.ex
@@ -47,40 +47,40 @@ defmodule Khf1 do
 
   
 
-  @spec correct?(pd::puzzle_desc()) :: b::boolean()
-  # b igaz, ha a pd feladványleíró helyes, egyébként hamis
+  # @spec correct?(pd::puzzle_desc()) :: b::boolean()
+  # # b igaz, ha a pd feladványleíró helyes, egyébként hamis
   
-  def correct?({n, m, fields}) do 
-    0 < n 
-    and 0 < m 
-    and m <= n
-    and correct_fields(n, m, fields)
-    and Enum.frequencies_by(fields,
-      fn {{row, col}, _} -> {row, col} end)
-      |> Map.values()
-      |> Enum.all?(fn x -> x == 1 end)
-  end
+  # def correct?({n, m, fields}) do 
+  #   0 < n 
+  #   and 0 < m 
+  #   and m <= n
+  #   and correct_fields(n, m, fields)
+  #   and Enum.frequencies_by(fields,
+  #     fn {{row, col}, _} -> {row, col} end)
+  #     |> Map.values()
+  #     |> Enum.all?(fn x -> x == 1 end)
+  # end
 
 
-  @spec correct_fields(n::integer(), m::integer(), pd::puzzle_desc()) :: b::boolean()
-  # b igaz, ha az alábbi feltételek teljesülnek:
-  # 0 < n, 0 < m ≤ n
-  # 0 < row ≤ n, 0 < col ≤ n
-  # 0 < val ≤ m
+  # @spec correct_fields(n::integer(), m::integer(), pd::puzzle_desc()) :: b::boolean()
+  # # b igaz, ha az alábbi feltételek teljesülnek:
+  # # 0 < n, 0 < m ≤ n
+  # # 0 < row ≤ n, 0 < col ≤ n
+  # # 0 < val ≤ m
   
-  defp correct_fields(_, _, []) do
-    true
-  end
+  # defp correct_fields(_, _, []) do
+  #   true
+  # end
 
-  defp correct_fields(n, m, [{{row, col}, val} | rest]) do
-    0 < row
-    and row <= n
-    and 0 < col 
-    and col <= n
-    and 0 < val
-    and val <= m
-    and correct_fields(n, m, rest)
-  end
+  # defp correct_fields(n, m, [{{row, col}, val} | rest]) do
+  #   0 < row
+  #   and row <= n
+  #   and 0 < col 
+  #   and col <= n
+  #   and 0 < val
+  #   and val <= m
+  #   and correct_fields(n, m, rest)
+  # end
     
 end
 
@@ -190,86 +190,6 @@ defmodule Khf2 do
   end
 end
 
-defmodule Khf3 do
-  @moduledoc """
-  Ciklikus számlisták
-  @author "Koczó Attila <koczoa@edu.bme.hu>"
-  @date   "2024-10-06"
-  ...
-  """
-  @type count() :: integer() # számsorozatok száma, n (1 < n)
-  @type cycle() :: integer() # számsorozat hossza, m (1 <= m)
-  @type size()  :: integer() # listahossz, len (1 < len)
-  @type value() :: integer() # listaelem értéke, val (0 <= val <= m)
-  @type index() :: integer() # listaelem sorszáma, ix (1 <= ix <= len)
-  @type index_value() :: {index(), value()} # listaelem indexe és értéke
-
-  @spec cyclists({n::count(), m::cycle(), len::size()}, constraints::[index_value()]) \
-    :: results::[[value()]]
-  # results az összes olyan len hosszú lista listája, melyekben
-  # * az 1-től m-ig tartó számsorozat – ebben a sorrendben, esetleg
-  #   közbeszúrt 0-kal – n-szer ismétlődik,
-  # * len-n*m számú helyen 0-k vannak,
-  # * a constraints korlát-listában felsorolt indexű cellákban a megadott
-  #   értékű elemek vannak.
-  def cyclists({n, m, len}, constraints) do    
-    backtrack(m, m, len, len-m*n, [], Map.new(constraints))
-  end
-
-  @spec putZeros(non::integer(), data::%{}, m::integer(), curr::integer()) :: results::[[value()]]
-  # visszaadja a nullások számát
-  defp putZeros(non, [], _, _) do
-    non
-  end
-  # ezeket kihagyja, mert a nullás korlátokkal nincs dolgunk itt
-  defp putZeros(non, [{_, 0} | rest], m, curr) do
-    putZeros(non, rest, m, curr)
-  end
-  # visszadja, hogy curr-ig mennyi darab nullást kell lerakni minimum
-  defp putZeros(non, [{cidx, val} | rest], m, curr) do
-    zz = non + Integer.mod((cidx - val - non), m)
-    if cidx <= curr do
-      putZeros(zz, rest, m, curr)
-    else
-      non
-    end
-  end
-  
-  @spec appender(val::integer(), m::integer(), len::integer(), non::integer(), data::%{}, cts::%{}, element::integer()) :: results::[[value()]]
-  # a lista elejére beteszi az element értékét, ami vagy egy 1..m szám, vagy egy nullás
-  defp appender(val, m, len, non, data, cts, element) do
-    if (cts[len] == nil or cts[len] == element) 
-      and (putZeros(0, cts |> Enum.to_list |> Enum.sort, m, len) < non+ 1) 
-    do
-      backtrack(val, m, len - 1, non, [element | data], cts)
-    else
-      []
-    end
-  end
-
-  @spec backtrack(val::integer(), m::integer(), len::integer(), non::integer(), data::%{}, cts::%{}) :: results::[[value()]]
-  # ha felépült a lista, és elfogytak a lerakandó nullák, akkor vissztér a listával
-  defp backtrack(_, _, 0, _, data, _) do
-    [data]
-  end
-  # megpróbálja felépíteni a listát hátúlról, és ha egy korlát nem teljesül, 
-  # akkor eldobja a próbálkozát
-  defp backtrack(val, m, len, non, data, cts) do
-    nextval = if val == 1 do 
-      m
-    else 
-      val - 1
-    end
-
-    cond do
-      non == len -> appender(val, m, len, non - 1, data, cts, 0)
-      non == 0 -> appender(nextval, m, len, non, data, cts, val)
-      true -> appender(val, m, len, non - 1, data, cts, 0) ++ appender(nextval, m, len, non, data, cts, val)
-    end
-  end
-end
-# Khf3.cyclists({3, 2, 7}, [{4, 0}])  |> IO.inspect
-
 defmodule Nhf1 do
   @moduledoc """
   Számtekercs
@@ -298,7 +218,7 @@ defmodule Nhf1 do
         solve(Khf2.balra(1, 1, n, 1, Enum.into(costs, %{})),  to2d(n, costs), 1, m)
       end
       
-      @spec to2d(n::integer(), consts::[index_value()])
+      # @spec to2d(n::integer(), consts::[index_value()]) :: %{}
       # előállít egy n*n-es "mátrixot", és a consts értékeit beleteszi
       defp to2d(n, consts) do
         cs = Map.new(consts)
@@ -309,7 +229,7 @@ defmodule Nhf1 do
         end          
       end
 
-      @spec solve(csiga::[field_opt_value()] , table::%{}, curr::integer(), m::integer())
+      # @spec solve(csiga::[field_opt_value()] , table::%{}, curr::integer(), m::integer()) :: solutions()
       # kitölti a table-t a megkapott csiga értékekkel
       defp solve([], table, _, _) do
         [table]
@@ -338,7 +258,7 @@ defmodule Nhf1 do
       end
 
 
-      @spec checkConst(table::%{}, coords::field(), m::integer(), val::integer())
+      # @spec checkConst(table::%{}, coords::field(), m::integer(), val::integer()) ::boolean()
       # ellenőrzi, hogy a val értéke lerakható-e a coords-ban kapott helyre a table-ben
       defp checkConst(table, {y, x}, m, val) do
         row = Enum.at(table, y-1)
@@ -353,7 +273,7 @@ defmodule Nhf1 do
         end
       end
       
-      @spec(curr::integer(), m::interger())
+      # @spec(curr::integer(), m::interger())
       # növeli egyel a curr értékét, amíg az el nem éri m-et, különben egyre állítja 
       defp currSetter(curr, m) do
         if curr == m do
@@ -362,43 +282,9 @@ defmodule Nhf1 do
           curr + 1
         end
       end
-      @spec replaceIdx(table::%{}, coords::field(), val::integer())
+      # @spec replaceIdx(table::%{}, coords::field(), val::integer()) ::%{}
       # A táblában az x, y koordinátán álló számot kicseréli a val értékére
       defp replaceIdx(table, {y, x}, val) do
         List.update_at(table, y-1, fn oldRow -> List.replace_at(oldRow, x-1, val) end)
       end
-end
-
-defmodule Nhf1Testcases do
-
-  testcases = # %{key => {size, cycle, constraints, solutions}}
-    %{
-      0 => {3, 2, [], [[[0, 1, 2], [1, 2, 0], [2, 0, 1]], [[0, 1, 2], [2, 0, 1], [1, 2, 0]], [[1, 2, 0], [2, 0, 1], [0, 1, 2]]]},
-      1 => {4, 2, [{{1, 1}, 1}, {{1, 4}, 2}], [[[1, 0, 0, 2], [0, 1, 2, 0], [0, 2, 1, 0], [2, 0, 0, 1]], [[1, 0, 0, 2], [2, 0, 0, 1], [0, 2, 1, 0], [0, 1, 2, 0]], [[1, 0, 0, 2], [2, 0, 1, 0], [0, 2, 0, 1], [0, 1, 2, 0]]]},
-      2 => {4, 1, [{{1, 1}, 1}], [[[1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0]], [[1, 0, 0, 0], [0, 0, 0, 1], [0, 1, 0, 0], [0, 0, 1, 0]], [[1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1], [0, 1, 0, 0]], [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]], [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]], [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]]},
-      3 => {4, 3, [], []},
-      4 => {5, 3, [{{1, 3}, 1}, {{2, 2}, 2}], [[[0, 0, 1, 2, 3], [0, 2, 0, 3, 1], [1, 3, 0, 0, 2], [3, 0, 2, 1, 0], [2, 1, 3, 0, 0]], [[0, 0, 1, 2, 3], [0, 2, 3, 0, 1], [1, 3, 0, 0, 2], [3, 0, 2, 1, 0], [2, 1, 0, 3, 0]]]},
-      5 => {6, 3, [{{1, 5}, 2}, {{2, 2}, 1}, {{4, 6}, 1}], [[[1, 0, 0, 0, 2, 3], [0, 1, 2, 3, 0, 0], [0, 3, 1, 2, 0, 0], [0, 2, 3, 0, 0, 1], [3, 0, 0, 0, 1, 2], [2, 0, 0, 1, 3, 0]]]},
-      6 => {6, 3, [{{1, 5}, 2}, {{2, 2}, 1}, {{4, 6}, 1}], [[[1, 0, 0, 0, 2, 3], [0, 1, 2, 3, 0, 0], [0, 3, 1, 2, 0, 0], [0, 2, 3, 0, 0, 1], [3, 0, 0, 0, 1, 2], [2, 0, 0, 1, 3, 0]]]},
-      7 => {6, 3, [{{2, 4}, 3}, {{3, 3}, 1}, {{3, 6}, 2}, {{6, 1}, 3}], [[[0, 1, 2, 0, 3, 0], [2, 0, 0, 3, 0, 1], [0, 3, 1, 0, 0, 2], [0, 0, 3, 2, 1, 0], [1, 0, 0, 0, 2, 3], [3, 2, 0, 1, 0, 0]]]},
-      8 => {7, 3, [{{1, 1}, 1}, {{2, 4}, 3}, {{3, 4}, 1}, {{4, 3}, 3}, {{6, 6}, 2}, {{7, 7}, 3}], [[[1, 0, 0, 2, 0, 3, 0], [0, 1, 2, 3, 0, 0, 0], [0, 3, 0, 1, 2, 0, 0], [0, 2, 3, 0, 0, 0, 1], [3, 0, 0, 0, 0, 1, 2], [0, 0, 1, 0, 3, 2, 0], [2, 0, 0, 0, 1, 0, 3]]]},
-      9 => {8, 3, [{{1, 4}, 1}, {{1, 7}, 3}, {{2, 3}, 2}, {{2, 4}, 3}, {{3, 2}, 1}, {{4, 7}, 1}, {{7, 7}, 2}], [[[0, 0, 0, 1, 0, 2, 3, 0], [0, 0, 2, 3, 0, 0, 0, 1], [0, 1, 0, 0, 2, 3, 0, 0], [0, 3, 0, 0, 0, 0, 1, 2], [1, 2, 3, 0, 0, 0, 0, 0], [3, 0, 0, 2, 0, 1, 0, 0], [0, 0, 1, 0, 3, 0, 2, 0], [2, 0, 0, 0, 1, 0, 0, 3]], [[0, 0, 0, 1, 0, 2, 3, 0], [0, 0, 2, 3, 0, 0, 0, 1], [0, 1, 0, 0, 2, 3, 0, 0], [0, 3, 0, 0, 0, 0, 1, 2], [1, 2, 3, 0, 0, 0, 0, 0], [3, 0, 0, 2, 1, 0, 0, 0], [0, 0, 1, 0, 3, 0, 2, 0], [2, 0, 0, 0, 0, 1, 0, 3]], [[0, 0, 0, 1, 0, 2, 3, 0], [0, 0, 2, 3, 0, 0, 0, 1], [0, 1, 0, 2, 0, 3, 0, 0], [0, 3, 0, 0, 0, 0, 1, 2], [1, 2, 3, 0, 0, 0, 0, 0], [3, 0, 0, 0, 2, 1, 0, 0], [0, 0, 1, 0, 3, 0, 2, 0], [2, 0, 0, 0, 1, 0, 0, 3]], [[0, 0, 0, 1, 0, 2, 3, 0], [0, 0, 2, 3, 0, 0, 0, 1], [0, 1, 0, 2, 3, 0, 0, 0], [0, 3, 0, 0, 0, 0, 1, 2], [1, 2, 3, 0, 0, 0, 0, 0], [3, 0, 0, 0, 2, 1, 0, 0], [0, 0, 0, 0, 1, 3, 2, 0], [2, 0, 1, 0, 0, 0, 0, 3]], [[0, 0, 0, 1, 0, 2, 3, 0], [0, 0, 2, 3, 0, 0, 0, 1], [0, 1, 0, 2, 3, 0, 0, 0], [0, 3, 0, 0, 0, 0, 1, 2], [1, 2, 3, 0, 0, 0, 0, 0], [3, 0, 0, 0, 2, 1, 0, 0], [0, 0, 1, 0, 0, 3, 2, 0], [2, 0, 0, 0, 1, 0, 0, 3]]]},
-      10 => {8, 4, [{{2, 3}, 4}, {{3, 3}, 2}, {{6, 1}, 1}, {{7, 6}, 3}], [[[0, 0, 1, 2, 3, 4, 0, 0], [0, 0, 4, 0, 1, 2, 3, 0], [0, 0, 2, 3, 0, 0, 4, 1], [3, 1, 0, 4, 0, 0, 0, 2], [2, 4, 0, 0, 0, 0, 1, 3], [1, 3, 0, 0, 0, 0, 2, 4], [0, 2, 0, 1, 4, 3, 0, 0], [4, 0, 3, 0, 2, 1, 0, 0]], [[0, 0, 1, 2, 3, 4, 0, 0], [0, 0, 4, 0, 1, 2, 3, 0], [3, 0, 2, 0, 0, 0, 4, 1], [0, 1, 3, 4, 0, 0, 0, 2], [2, 4, 0, 0, 0, 0, 1, 3], [1, 3, 0, 0, 0, 0, 2, 4], [0, 2, 0, 1, 4, 3, 0, 0], [4, 0, 0, 3, 2, 1, 0, 0]]]},
-      11 => {9, 3, [{{1, 7}, 3}, {{3, 1}, 1}, {{6, 1}, 3}, {{6, 2}, 2}, {{6, 6}, 1}, {{8, 4}, 3}, {{9, 2}, 1}], [[[0, 0, 0, 0, 1, 2, 3, 0, 0], [0, 0, 2, 0, 0, 0, 0, 3, 1], [1, 3, 0, 0, 0, 0, 0, 0, 2], [0, 0, 0, 1, 2, 3, 0, 0, 0], [0, 0, 0, 2, 3, 0, 1, 0, 0], [3, 2, 0, 0, 0, 1, 0, 0, 0], [0, 0, 3, 0, 0, 0, 2, 1, 0], [0, 0, 1, 3, 0, 0, 0, 2, 0], [2, 1, 0, 0, 0, 0, 0, 0, 3]]]}
-    }
-  for k <- 1..100 do
-    for j <- 1..10 do
-        
-      for i <- 0..map_size(testcases)-1
-        do
-        {size, cycle, constrains, solutions} = testcases[i]
-        {"Test case #{i}",
-         Nhf1.helix({size, cycle, constrains}) |> Enum.sort() === solutions
-        }
-        # |> IO.inspect
-      end
-    end
-  IO.inspect(k)
-  end
-  
 end
